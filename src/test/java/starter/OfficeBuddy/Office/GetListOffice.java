1c8031b21a6bf02211862fadc0217afcb14e3649
riskadwinuraini <74886545+riskadwinuraini@users.noreply.github.com>
2023-06-13 23:12:00 +0700

    update payment detail

--- src/test/java/starter/OfficeBuddy/Office/GetListOffice.java
+++ src/test/java/starter/OfficeBuddy/Office/GetListOffice.java
@@ -18,7 +18,7 @@ public class GetListOffice {
         SerenityRest.given().header("Authorization","Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluMUBnbWFpbC5jb20iLCJpZCI6MTMsImlzX3ZlcmlmeSI6ZmFsc2UsInJvbGUiOjF9.CVDdRQKKNo6YJsrBH3s90xj09e6-vGB9lSBld4bCWE0")
                 .get(GetApiEndpoint());
     }
-    @Step("I receive valid HTTP response code 200")
+    @Step("I receive valid HTTP response code 200 for get detail payment")
     public void validateHttpResponseCode200(){
 
         restAssuredThat(response -> response.statusCode(200));
