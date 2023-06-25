e9d6644464145f937c9438555e1cf9b2092af26f
L200190245_IkhsanNurazis <ikhsannurazis22@gmail.com>
2023-06-25 19:41:29 +0700

    update dikit

--- src/test/java/starter/StepDefinition/UserSteps.java
+++ src/test/java/starter/StepDefinition/UserSteps.java
@@ -1,11 +1,11 @@
-package starter.StepDefenition;
+package starter.StepDefinition;
 
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import net.thucydides.core.annotations.Steps;
-import starter.officebuddy.User.*;
+import starter.OfficeBuddy.User.*;
 
 public class UserSteps {
     @Steps
